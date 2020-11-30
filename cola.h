d6c567a32ccbb49a2977a934fcc972cbed502710
Michel Dávalos <michel.davalos@academicos.udg.mx>
2020-11-30 13:24:58 -0600

    creando repositorio

---
+++ cola.h
@@ -0,0 +1,6 @@
+#ifndef COLA_H
+#define COLA_H
+
+// Implementar cola dinamica con listadobleligada.h
+
+#endif
---
+++ pila.h
@@ -0,0 +1,6 @@
+#ifndef PILA_H
+#define PILA_H
+
+// Implementar pila dinamica con listadobleligada.h
+
+#endif
